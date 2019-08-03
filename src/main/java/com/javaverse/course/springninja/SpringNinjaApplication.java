f0ae520388195f22630ff60c3315907ad366251f
NLGRF <non.devtech@gmail.com>
2019-08-03 16:12:49 +0700

    [Step 5] Logging using Log4j2 & Tuning Up !

--- src/main/java/com/javaverse/course/springninja/SpringNinjaApplication.java
+++ src/main/java/com/javaverse/course/springninja/SpringNinjaApplication.java
@@ -1,13 +1,29 @@
 package com.javaverse.course.springninja;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@Log4j2
 @SpringBootApplication
 public class SpringNinjaApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringNinjaApplication.class, args);
+    public static String expensiveMethod(String level) {
+        System.out.println("Perform Expensive Method : " + level);
+        return level;
     }
 
+    public static void main(String[] args) {
+        System.err.close();
+        SpringApplication.run(SpringNinjaApplication.class, args);
+
+        log.trace("{} Message", () -> expensiveMethod("TRACE"));
+        log.debug("{} Message", () -> expensiveMethod("DEBUG"));
+        log.info("{} Message", () -> expensiveMethod("INFO"));
+        log.warn("{} Message", () -> expensiveMethod("WARN"));
+        log.error("{} Message", () -> expensiveMethod("ERROR"));
+        log.fatal("{} Message", () -> expensiveMethod("FATAL"));
+
+    }
 }
